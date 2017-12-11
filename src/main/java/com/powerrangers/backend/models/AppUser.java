33cf613b1da60e30ccd462d0f0aa5429163b911e
Maks Muzychuk <mak.muzychuk@gmail.com>
2017-12-11 22:37:59 +0200

    Initial commit. Created AppUser model

---
+++ src/main/java/com/powerrangers/backend/models/AppUser.java
@@ -0,0 +1,82 @@
+package com.powerrangers.backend.models;
+/*
+/AppUser class for user presenting
+ */
+
+public class AppUser {
+
+    private String email;
+    private String password;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String resetToken;
+
+    private int age;
+
+    private boolean active = false;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getResetToken() {
+        return resetToken;
+    }
+
+    public void setResetToken(String resetToken) {
+        this.resetToken = resetToken;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
